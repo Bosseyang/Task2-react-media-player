8d72864fe9052443426873b89477117a2332a6f4
Bosseyang <bosse_yang@hotmail.com>
2025-08-19 16:32:18 +0200

    Create songsection.tsx

---
+++ react-media-player/src/components/songsection.tsx
@@ -0,0 +1,9 @@
+import type { ReactElement } from "react";
+
+export default function SongSection(): ReactElement {
+  return (
+    <section className="section song-section" id="song-section">
+      <h1>Song Info goes here</h1>
+    </section>
+  );
+}
