2fc99b530f786b7aefaddeb240f7f31861a22edd
Bosseyang <bosse_yang@hotmail.com>
2025-08-19 16:32:32 +0200

    Update main.tsx

--- react-media-player/src/main.tsx
+++ react-media-player/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./css/index.css";
+import Header from "./components/header";
+import Playlist from "./components/playlist";
+import SongSection from "./components/songsection";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Header />
+    <main className="main">
+      <Playlist />
+      <SongSection />
+    </main>
+  </StrictMode>
+);
