71b3961a613016a5ec6859700ea14301b73d849f
Bosseyang <bosse_yang@hotmail.com>
2025-08-19 16:32:16 +0200

    Create playlist.tsx

---
+++ react-media-player/src/components/playlist.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from "react";
+
+export default function Playlist(): ReactElement {
+  return (
+    <section className="section playlist-section" id="playlist-section">
+      <div>
+        <p>Some Song here!</p>
+      </div>
+    </section>
+  );
+}
