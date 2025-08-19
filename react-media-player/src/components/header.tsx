c9d5748f02e5c9351d4b7f576009ead64908f91f
Bosseyang <bosse_yang@hotmail.com>
2025-08-19 16:32:14 +0200

    Create header.tsx

---
+++ react-media-player/src/components/header.tsx
@@ -0,0 +1,17 @@
+import type { ReactElement } from "react";
+
+export default function Header(): ReactElement {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <a href="" className="header-button">
+          <span className="material-symbols-outlined">arrow_back_ios</span>
+        </a>
+        <h1 className="header-h1">Playlist - Moody</h1>
+        <a href="" className="header-button">
+          <span className="material-symbols-outlined">more_vert</span>
+        </a>
+      </nav>
+    </header>
+  );
+}
