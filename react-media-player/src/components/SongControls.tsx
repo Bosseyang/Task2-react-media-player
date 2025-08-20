4e77a29553cb1315e2ea1c1de5a260d2581ade34
Bosseyang <bosse_yang@hotmail.com>
2025-08-20 16:00:54 +0200

    Update SongControls.tsx

--- react-media-player/src/components/SongControls.tsx
+++ react-media-player/src/components/SongControls.tsx
@@ -3,11 +3,23 @@ import type { ReactElement } from "react";
 export default function Controls(): ReactElement {
   return (
     <section className="controls">
+      <span className="button repeat-button material-symbols-outlined">
+        repeat
+      </span>
       <div className="center-controls">
+        <span className="button rewind-button material-symbols-outlined">
+          fast_rewind
+        </span>
         <span className="button play-button material-symbols-outlined">
           play_arrow
         </span>
+        <span className="button forward-button material-symbols-outlined">
+          fast_forward
+        </span>
       </div>
+      <span className="button shuffle-button material-symbols-outlined">
+        shuffle
+      </span>
     </section>
   );
 }
