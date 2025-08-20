d2e9fe28b037660f1f781b24db92a41c9d40144a
Bosseyang <bosse_yang@hotmail.com>
2025-08-20 10:45:18 +0200

    Update songcard.tsx

--- react-media-player/src/components/songcard.tsx
+++ react-media-player/src/components/songcard.tsx
@@ -1,20 +1,18 @@
 import type { ReactElement } from "react";
-interface ISongCardProps {
-  artist: string;
-  title: string;
-  image: string;
-}
+import type { ISongData } from "../assets/songdata";
 
-export default function SongCard(props: ISongCardProps): ReactElement {
-  const { artist, title, image } = props;
+export default function SongCard(props: ISongData): ReactElement {
+  const { artist, title, image, songId } = props;
   return (
-    <section className="section songcard-section" id="song1">
+    <section className="section songcard-section" id={songId}>
       <img className="songcard-img" src={image} alt={title} />
       <section className="section song-title">
         <h1 className="song-h1">{artist}</h1>
         <h2 className="song-h2">{title}</h2>
       </section>
-      <span className="play-button material-symbols-outlined">play_arrow</span>
+      <span className="button play-button material-symbols-outlined">
+        play_arrow
+      </span>
     </section>
   );
 }
