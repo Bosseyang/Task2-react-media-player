31f51382b821e57191d9757c3a3c2bafba3cea85
Bosseyang <bosse_yang@hotmail.com>
2025-08-20 10:44:25 +0200

    Update songdata.tsx
    add songId

--- react-media-player/src/assets/songdata.tsx
+++ react-media-player/src/assets/songdata.tsx
@@ -2,36 +2,43 @@ export interface ISongData {
   artist: string;
   title: string;
   image: string;
+  songId: string;
 }
 export type SongData = ISongData[];
 
 export const songs: SongData = [
   {
+    songId: "1",
     artist: "A Rocket to the moon",
     title: "Like we used to",
     image: "/songImage/A_rocket_to_the_moon.jpg",
   },
   {
+    songId: "2",
     artist: "John Legend",
     title: "Save the room",
     image: "/songImage/Save_Room_John_Legend.jpg",
   },
   {
+    songId: "3",
     artist: "John Legend",
     title: "We just don't care",
     image: "/songImage/once_again.jpg",
   },
   {
+    songId: "4",
     artist: "A Rocket to the moon",
     title: "Dakota",
     image: "/songImage/A_rocket_to_the_moon.jpg",
   },
   {
+    songId: "5",
     artist: "Zedd & Aloe Blacc",
     title: "Candyman",
     image: "/songImage/candyman.jpg",
   },
   {
+    songId: "6",
     artist: "Rag'n'Bone Man",
     title: "Human",
     image: "/songImage/human.jpg",
