14771b0ab6520e4fe66061117b861bbfb1d2e040
Bosseyang <bosse_yang@hotmail.com>
2025-08-20 14:22:58 +0200

    Update SongCard.tsx

--- react-media-player/src/components/SongCard.tsx
+++ react-media-player/src/components/SongCard.tsx
@@ -1,10 +1,21 @@
-import type { ReactElement } from "react";
+import { useState, type ReactElement } from "react";
 import type { ISongData } from "../assets/songdata";
 
 export default function SongCard(props: ISongData): ReactElement {
   const { artist, title, image, songId } = props;
+
+  const [focus, setFocus] = useState(songId);
+
+  const onFocus = () => {
+    setFocus(songId);
+    console.log(focus);
+  };
   return (
-    <section className="section songcard-section" id={songId}>
+    <section
+      onClick={onFocus}
+      className={`section songcard-section songId-${onFocus}`}
+      key={songId}
+    >
       <img className="songcard-img" src={image} alt={title} />
       <section className="section song-title">
         <h1 className="song-h1">{artist}</h1>
