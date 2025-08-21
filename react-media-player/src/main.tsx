d932dfcacba672c9a0df266f57750c0d3306894c
Bosseyang <bosse_yang@hotmail.com>
2025-08-21 14:41:59 +0200

    Update main.tsx

--- react-media-player/src/main.tsx
+++ react-media-player/src/main.tsx
@@ -4,7 +4,7 @@ import "./css/index.css";
 import Header from "./components/Header";
 import Playlist from "./components/Playlist";
 import SongSection from "./components/SongSection";
-
+// const [selectedTrack, setSelectedTrack] = useState<ITrack | null>(null);
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Header />
