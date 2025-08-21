d96461cc33e584e9dbf55957a226bc665bf2de27
Bosseyang <bosse_yang@hotmail.com>
2025-08-21 14:54:05 +0200

    Update songdata.tsx

--- react-media-player/src/assets/songdata.tsx
+++ react-media-player/src/assets/songdata.tsx
@@ -2,57 +2,66 @@ export interface ISongData {
   artist: string;
   title: string;
   image: string;
-  songId: number;
+  id: number;
+  duration: number;
 }
 export type SongData = ISongData[];
 
 export const songs: SongData = [
   {
-    songId: 1,
+    id: 1,
     artist: "A Rocket to the moon",
     title: "Like we used to",
     image: "/songImage/A_rocket_to_the_moon.jpg",
+    duration: 220,
   },
   {
-    songId: 2,
+    id: 2,
     artist: "John Legend",
     title: "Save the room",
     image: "/songImage/Save_Room_John_Legend.jpg",
+    duration: 220,
   },
   {
-    songId: 3,
+    id: 3,
     artist: "John Legend",
     title: "We just don't care",
     image: "/songImage/once_again.jpg",
+    duration: 220,
   },
   {
-    songId: 4,
+    id: 4,
     artist: "A Rocket to the moon",
     title: "Dakota",
     image: "/songImage/A_rocket_to_the_moon.jpg",
+    duration: 220,
   },
   {
-    songId: 5,
+    id: 5,
     artist: "Zedd & Aloe Blacc",
     title: "Candyman",
     image: "/songImage/candyman.jpg",
+    duration: 220,
   },
   {
-    songId: 6,
+    id: 6,
     artist: "Rag'n'Bone Man",
     title: "Human",
     image: "/songImage/human.jpg",
+    duration: 220,
   },
   {
-    songId: 7,
+    id: 7,
     artist: "Imagine Dragons",
     title: "Shots (Broiler Remix)",
     image: "/songImage/imagine_dragons_shots.jpg",
+    duration: 220,
   },
   {
-    songId: 8,
+    id: 8,
     artist: "Imagine Dragons",
     title: "Demons",
     image: "/songImage/imagine_dragons_demons.jpg",
+    duration: 220,
   },
 ];
