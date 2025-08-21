c488b1989ba085700502c257a7e94e80d3ef120e
Bosseyang <bosse_yang@hotmail.com>
2025-08-21 14:41:50 +0200

    Update Playlist.tsx

--- react-media-player/src/components/Playlist.tsx
+++ react-media-player/src/components/Playlist.tsx
@@ -1,27 +1,27 @@
 import { useState, type ReactElement } from "react";
 import SongCard from "./SongCard";
-import { songs } from "../assets/songdata";
-// import type { ITrack } from "../types";
+import { songs, type SongData } from "../assets/songdata";
+import type { ISong } from "../types";
 
 export default function Playlist(): ReactElement {
   // export const Playlist: React.FC<{
-  //   songs: SongData[];
-  //   onSelect: (track: SongData) => void;
+  //   songs: ISong[];
+  //   onSelect: (song: ISong) => void;
   // }> = ({ songs, onSelect }) => {
 
   return (
     <section className="section playlist-section" id="playlist-section">
-      <div>
-        {songs.map((song) => (
-          <SongCard
-            key={song.songId}
-            artist={song.artist}
-            title={song.title}
-            image={song.image}
-            songId={song.songId}
-          />
-        ))}
-      </div>
+      {songs.map((song) => (
+        // <div onClick={() => onselect(song)}>
+        <SongCard
+          key={song.songId}
+          artist={song.artist}
+          title={song.title}
+          image={song.image}
+          songId={song.songId}
+        />
+        // </div>
+      ))}
     </section>
   );
 }
