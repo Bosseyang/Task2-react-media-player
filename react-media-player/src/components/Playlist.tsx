e4a8fd8b6d925515dfa5724ba9878338173ab2c2
Bosseyang <bosse_yang@hotmail.com>
2025-08-22 11:01:20 +0200

    Update Playlist.tsx
    Add functionality, right song shows up on click

--- react-media-player/src/components/Playlist.tsx
+++ react-media-player/src/components/Playlist.tsx
@@ -1,27 +1,25 @@
-import { useState, type ReactElement } from "react";
+import { type FC } from "react";
 import SongCard from "./SongCard";
-import { songs, type SongData } from "../assets/songdata";
+
 import type { ISong } from "../types";
 
-export default function Playlist(): ReactElement {
-  // export const Playlist: React.FC<{
-  //   songs: ISong[];
-  //   onSelect: (song: ISong) => void;
-  // }> = ({ songs, onSelect }) => {
-
+export const Playlist: FC<{
+  songs: ISong[];
+  onSelect: (song: ISong) => void;
+}> = ({ songs, onSelect }) => {
   return (
     <section className="section playlist-section" id="playlist-section">
       {songs.map((song) => (
-        // <div onClick={() => onselect(song)}>
-        <SongCard
-          key={song.id}
-          artist={song.artist}
-          title={song.title}
-          image={song.image}
-          id={song.id}
-        />
-        // </div>
+        <div onClick={() => onSelect(song)}>
+          <SongCard
+            key={song.id}
+            artist={song.artist}
+            title={song.title}
+            image={song.image}
+            id={song.id}
+          />
+        </div>
       ))}
     </section>
   );
-}
+};
